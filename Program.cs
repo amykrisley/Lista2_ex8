96acbc754f2be955ade89a2ea076ce2b7bf5cced
amykrisley <130614797+amykrisley@users.noreply.github.com>
2023-05-03 18:31:10 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lisat2_ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+
+            Console.Write("Digite é o 1° valor ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite é o 2° valor ");
+            b = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite é o 3° valor ");
+            c = double.Parse(Console.ReadLine());
+
+            if (Math.Pow(a, 2) == (Math.Pow(b, 2) + Math.Pow(c, 2)) ||
+                Math.Pow(b, 2) == (Math.Pow(a, 2) + Math.Pow(c, 2)) ||
+                Math.Pow(c, 2) == (Math.Pow(a, 2) + Math.Pow(b, 2)))
+
+                Console.WriteLine("Forma um triângulo retângulo");
+            else
+                Console.WriteLine("Não forma um triângulo retângulo");
+        }
+    }
+}
